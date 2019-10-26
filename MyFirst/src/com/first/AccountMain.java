9630a663c035d1c96c95f72001a4919110838f77
navneet1996 <57025976+navneet1996@users.noreply.github.com>
2019-10-26 10:00:37 +0530

    maya changed

--- MyFirst/src/com/first/AccountMain.java
+++ MyFirst/src/com/first/AccountMain.java
@@ -2,11 +2,12 @@ package com.first;
 
 public class AccountMain {
 
+	//changes made by maya sysout to a1
 	public static void main(String[] args) {
 
 Account a=new Account(1,"sbi");
 Account a1=new Account(2,"pnb");
-System.out.println(a);
+System.out.println(a1);
 
 	}
 
