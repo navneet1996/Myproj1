445b475bc6ea274ad2ae4c8ce7240a2198e745e1
navneet.saini <navneet.saini@WKSBAN24KAN0020.corp.mphasis.com>
2019-10-26 09:58:48 +0530

    navneet changes

---
+++ MyFirst/src/com/first/AccountMain.java
@@ -0,0 +1,13 @@
+package com.first;
+
+public class AccountMain {
+
+	public static void main(String[] args) {
+
+Account a=new Account(1,"sbi");
+Account a1=new Account(2,"pnb");
+System.out.println(a);
+
+	}
+
+}
